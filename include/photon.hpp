205c33fd97d190160db8723cfcbdad54198a45ed
Anjan Roy <hello@itzmeanjan.in>
2022-06-08 15:06:31 +0400

    apply 4-bit S-box to each of 64 cells of permutation state
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/photon.hpp
+++ include/photon.hpp
@@ -8,8 +8,7 @@ namespace photon {
 // https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/photon-beetle-spec-final.pdf
 constexpr size_t ROUNDS = 12ul;
 
-// Photon256 permutation's round constants, taken from Photon-Bettle AEAD
-// specification
+// Photon256 permutation's round constants, see Photon-Bettle AEAD specification
 // https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/photon-beetle-spec-final.pdf
 constexpr uint8_t RC[96] = {
   1,  0,  2,  6,  14, 15, 13, 9,  3,  2,  0, 4,  12, 13, 15, 11, 7,  6,  4,  0,
@@ -19,6 +18,12 @@ constexpr uint8_t RC[96] = {
   5,  4,  6,  2,  10, 11, 9,  13, 10, 11, 9, 13, 5,  4,  6,  2
 };
 
+// 4 -bit S-box applied to each cell of 8x8 permutation state matrix, see
+// Photon-Bettle AEAD specification
+// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/photon-beetle-spec-final.pdf
+constexpr uint8_t SBOX[16] = { 0xC, 0x5, 0x6, 0xB, 0x9, 0x0, 0xA, 0xD,
+                               0x3, 0xE, 0xF, 0x8, 0x4, 0x7, 0x1, 0x2 };
+
 // Compile-time evaluation to check round index is valid
 static inline constexpr bool
 check_r(const size_t r)
@@ -26,7 +31,9 @@ check_r(const size_t r)
   return r < ROUNDS;
 }
 
-// Add fixed constants to the cells of first column of 8x8 permutation state
+// Add fixed constants to the cells of first column of 8x8 permutation state,
+// see figure 2.1 of Photon-Bettle AEAD specification
+// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/photon-beetle-spec-final.pdf
 template<const size_t r>
 inline static void
 add_constant(
@@ -42,6 +49,18 @@ add_constant(
 #endif
   for (size_t i = 0; i < 8; i++) {
     state[i << 3] ^= RC[off ^ i];
+  }
+}
+
+// Applies 4 -bit S-box to each cell of 8x8 permutation state, see figure 2.1 of
+// Photon-Bettle AEAD specification
+// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/photon-beetle-spec-final.pdf
+inline static void
+subcells(uint8_t* const __restrict state // 8x8 permutation state ( 256 -bits )
+)
+{
+  for (size_t i = 0; i < 64; i++) {
+    state[i] = SBOX[state[i]];
   }
 }
 
