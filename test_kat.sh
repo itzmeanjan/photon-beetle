d53974dc84a97514fca7f777134be8c73654a267
Anjan Roy <hello@itzmeanjan.in>
2022-06-11 17:29:57 +0400

    test functional correctness of photon-beetle-aead[32, 128]
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- test_kat.sh
+++ test_kat.sh
@@ -10,13 +10,15 @@ make lib
 mkdir -p tmp
 pushd tmp
 
-# download compressed NIST LWC submission of Sparkle
+# download compressed NIST LWC submission of Photon-Beetle
 wget -O photon-beetle.zip https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-submissions/photon-beetle.zip
 # uncomress
 unzip photon-beetle.zip
 
 # copy Known Answer Tests outside of uncompressed NIST LWC submission directory
 cp photon-beetle/Implementations/crypto_hash/photonbeetlehash256rate32v1/LWC_HASH_KAT_256.txt ../
+cp photon-beetle/Implementations/crypto_aead/photonbeetleaead128rate32v1/LWC_AEAD_KAT_128_128.txt ../LWC_AEAD_KAT_128_128.txt.32
+cp photon-beetle/Implementations/crypto_aead/photonbeetleaead128rate128v1/LWC_AEAD_KAT_128_128.txt ../LWC_AEAD_KAT_128_128.txt.128
 
 popd
 
@@ -25,15 +27,19 @@ popd
 # remove NIST LWC submission zip
 rm -rf tmp
 
-# move Known Answer Tests to execution directory
-mv LWC_HASH_KAT_256.txt wrapper/python/
-
 # ---
 
 pushd wrapper/python
 
 # run tests
-python3 -m pytest -v
+mv ../../LWC_HASH_KAT_256.txt .
+python3 -m pytest -k hash_kat --cache-clear -v
+
+mv ../../LWC_AEAD_KAT_128_128.txt.32 LWC_AEAD_KAT_128_128.txt
+python3 -m pytest -k aead_32_kat --cache-clear -v
+
+mv ../../LWC_AEAD_KAT_128_128.txt.128 LWC_AEAD_KAT_128_128.txt
+python3 -m pytest -k aead_128_kat --cache-clear -v
 
 # clean up
 rm LWC_*_KAT_*.txt
--- wrapper/python/test_photon_beetle.py
+++ wrapper/python/test_photon_beetle.py
@@ -39,10 +39,134 @@ def test_photon_beetle_hash_kat():
 
             digest = pb.photon_beetle_hash(msg)
 
-            assert (
-                md == digest
-            ), f"[Photon-Beetle-Hash KAT {cnt}] expected {md}, found {digest} !"
+            assert (md == digest), f"[Photon-Beetle-Hash KAT {cnt}] expected {md}, found {digest} !"
 
+            fd.readline()
+
+
+def test_photon_beetle_aead_32_kat():
+    """
+    Tests functional correctness of Photon-Beetle-AEAD[32] implementation, using
+    Known Answer Tests submitted along with final round submission of `photon-beetle` in NIST LWC
+    See https://csrc.nist.gov/projects/lightweight-cryptography/finalists
+    """
+    with open("LWC_AEAD_KAT_128_128.txt", "r") as fd:
+        while True:
+            cnt = fd.readline()
+            if not cnt:
+                # no more KATs remaining
+                break
+
+            key = fd.readline()
+            nonce = fd.readline()
+            pt = fd.readline()
+            ad = fd.readline()
+            ct = fd.readline()
+
+            # extract out required fields
+            cnt = int([i.strip() for i in cnt.split("=")][-1])
+            key = [i.strip() for i in key.split("=")][-1]
+            nonce = [i.strip() for i in nonce.split("=")][-1]
+            pt = [i.strip() for i in pt.split("=")][-1]
+            ad = [i.strip() for i in ad.split("=")][-1]
+            ct = [i.strip() for i in ct.split("=")][-1]
+
+            # 128 -bit secret key
+            key = int(f"0x{key}", base=16).to_bytes(16, "big")
+            # 128 -bit public message nonce
+            nonce = int(f"0x{nonce}", base=16).to_bytes(16, "big")
+            # plain text
+            pt = bytes(
+                [
+                    int(f"0x{pt[(i << 1): ((i+1) << 1)]}", base=16)
+                    for i in range(len(pt) >> 1)
+                ]
+            )
+            # associated data
+            ad = bytes(
+                [
+                    int(f"0x{ad[(i << 1): ((i+1) << 1)]}", base=16)
+                    for i in range(len(ad) >> 1)
+                ]
+            )
+            # cipher text + authentication tag ( expected )
+            ct = bytes(
+                [
+                    int(f"0x{ct[(i << 1): ((i+1) << 1)]}", base=16)
+                    for i in range(len(ct) >> 1)
+                ]
+            )
+
+            cipher, tag = pb.photon_beetle_32_encrypt(key, nonce, ad, pt)
+            flag, text = pb.photon_beetle_32_decrypt(key, nonce, tag, ad, cipher)
+
+            assert (cipher + tag == ct), f"[Photon-Beetle-AEAD[32] KAT {cnt}] expected cipher to be 0x{ct.hex()}, found 0x{(cipher + tag).hex()} !"
+            assert (pt == text and flag), f"[Photon-Beetle-AEAD[32] KAT {cnt}] expected plain text 0x{pt.hex()}, found 0x{text.hex()} !"
+
+            # don't need this line, so discard
+            fd.readline()
+
+
+def test_photon_beetle_aead_128_kat():
+    """
+    Tests functional correctness of Photon-Beetle-AEAD[128] implementation, using
+    Known Answer Tests submitted along with final round submission of `photon-beetle` in NIST LWC
+    See https://csrc.nist.gov/projects/lightweight-cryptography/finalists
+    """
+    with open("LWC_AEAD_KAT_128_128.txt", "r") as fd:
+        while True:
+            cnt = fd.readline()
+            if not cnt:
+                # no more KATs remaining
+                break
+
+            key = fd.readline()
+            nonce = fd.readline()
+            pt = fd.readline()
+            ad = fd.readline()
+            ct = fd.readline()
+
+            # extract out required fields
+            cnt = int([i.strip() for i in cnt.split("=")][-1])
+            key = [i.strip() for i in key.split("=")][-1]
+            nonce = [i.strip() for i in nonce.split("=")][-1]
+            pt = [i.strip() for i in pt.split("=")][-1]
+            ad = [i.strip() for i in ad.split("=")][-1]
+            ct = [i.strip() for i in ct.split("=")][-1]
+
+            # 128 -bit secret key
+            key = int(f"0x{key}", base=16).to_bytes(16, "big")
+            # 128 -bit public message nonce
+            nonce = int(f"0x{nonce}", base=16).to_bytes(16, "big")
+            # plain text
+            pt = bytes(
+                [
+                    int(f"0x{pt[(i << 1): ((i+1) << 1)]}", base=16)
+                    for i in range(len(pt) >> 1)
+                ]
+            )
+            # associated data
+            ad = bytes(
+                [
+                    int(f"0x{ad[(i << 1): ((i+1) << 1)]}", base=16)
+                    for i in range(len(ad) >> 1)
+                ]
+            )
+            # cipher text + authentication tag ( expected )
+            ct = bytes(
+                [
+                    int(f"0x{ct[(i << 1): ((i+1) << 1)]}", base=16)
+                    for i in range(len(ct) >> 1)
+                ]
+            )
+
+            cipher, tag = pb.photon_beetle_128_encrypt(key, nonce, ad, pt)
+            flag, text = pb.photon_beetle_128_decrypt(key, nonce, tag, ad, cipher)
+
+            # assert (cipher + tag == ct), f"[Photon-Beetle-AEAD[128] KAT {cnt}] expected cipher to be 0x{ct.hex()}, found 0x{(cipher + tag).hex()} !"
+            assert (pt == text and flag), f"[Photon-Beetle-AEAD[128] KAT {cnt}] expected plain text 0x{pt.hex()}, found 0x{text.hex()} !"
+
+            # don't need this line, so discard
             fd.readline()
 
 
