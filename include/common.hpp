e336511b2f5c26fa6d431443e4969b3f23078ef3
Anjan Roy <hello@itzmeanjan.in>
2022-06-13 17:21:17 +0400

    fixed photon-beetle-aead-128 's rate rotation routine
    Placing permutation state bytes in little endian order and then rotating
    solves the issue of wrong cipher text generated.
    
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/common.hpp
+++ include/common.hpp
@@ -122,22 +122,19 @@ shuffle(const uint8_t* const __restrict state,
         uint8_t* const __restrict shuffled) requires(check_po2(RATE))
 {
   if constexpr (RATE == 4ul) {
-    const uint16_t s1 = (static_cast<uint16_t>(state[1] & photon::LS4B) << 12) |
-                        (static_cast<uint16_t>(state[0] & photon::LS4B) << 8) |
-                        (static_cast<uint16_t>(state[3] & photon::LS4B) << 4) |
-                        (static_cast<uint16_t>(state[2] & photon::LS4B) << 0);
+    const uint16_t s1 = (static_cast<uint16_t>(state[3] & photon::LS4B) << 12) |
+                        (static_cast<uint16_t>(state[2] & photon::LS4B) << 8) |
+                        (static_cast<uint16_t>(state[1] & photon::LS4B) << 4) |
+                        (static_cast<uint16_t>(state[0] & photon::LS4B) << 0);
 
     const uint16_t s1_prime = std::rotr(s1, 1);
 
-    shuffled[0] = state[4];
-    shuffled[1] = state[5];
-    shuffled[2] = state[6];
-    shuffled[3] = state[7];
+    std::memcpy(shuffled, state + RATE, RATE);
 
-    shuffled[4] = static_cast<uint8_t>(s1_prime >> 8) & photon::LS4B;
-    shuffled[5] = static_cast<uint8_t>(s1_prime >> 12) & photon::LS4B;
-    shuffled[6] = static_cast<uint8_t>(s1_prime >> 0) & photon::LS4B;
-    shuffled[7] = static_cast<uint8_t>(s1_prime >> 4) & photon::LS4B;
+    shuffled[4] = static_cast<uint8_t>(s1_prime >> 0) & photon::LS4B;
+    shuffled[5] = static_cast<uint8_t>(s1_prime >> 4) & photon::LS4B;
+    shuffled[6] = static_cast<uint8_t>(s1_prime >> 8) & photon::LS4B;
+    shuffled[7] = static_cast<uint8_t>(s1_prime >> 12) & photon::LS4B;
   } else if constexpr (RATE == 16ul) {
     constexpr size_t CNT = RATE >> 1;
 
@@ -145,7 +142,7 @@ shuffle(const uint8_t* const __restrict state,
 
     for (size_t i = 0; i < CNT; i++) {
       const size_t soff = i << 1;
-      const size_t shift = (7 - i) << 3;
+      const size_t shift = i << 3;
 
       const uint8_t w = (state[soff ^ 1] << 4) | (state[soff] & photon::LS4B);
 
@@ -154,14 +151,7 @@ shuffle(const uint8_t* const __restrict state,
 
     const uint64_t s1_prime = std::rotr(s1, 1);
 
-#if defined __clang__
-#pragma unroll 16
-#elif defined __GNUG__
-#pragma GCC unroll 16
-#endif
-    for (size_t i = 0; i < RATE; i++) {
-      shuffled[i] = state[RATE ^ i];
-    }
+    std::memcpy(shuffled, state + RATE, RATE);
 
 #if defined __clang__
 #pragma unroll 8
@@ -170,7 +160,7 @@ shuffle(const uint8_t* const __restrict state,
 #endif
     for (size_t i = 0; i < CNT; i++) {
       const size_t soff = RATE ^ (i << 1);
-      const size_t shift = (7 - i) << 3;
+      const size_t shift = i << 3;
 
       const uint8_t w = static_cast<uint8_t>(s1_prime >> shift);
 
