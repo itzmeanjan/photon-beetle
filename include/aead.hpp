c89d50154746aaef74b1d2e24e14a8ed8218a818
Anjan Roy <hello@itzmeanjan.in>
2023-01-23 15:35:37 +0400

    rewrite photon-beetle-aead encrypt routine using permutation state matrix of dim 8x4
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/aead.hpp
+++ include/aead.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include "common.hpp"
+#include "photon.hpp"
+#include <cstring>
 
 // Photon-Beetle-{Hash, AEAD} function(s)
 namespace photon_beetle {
@@ -85,6 +87,68 @@ encrypt(
   }
 
   gen_tag<16ul>(state, tag);
+}
+
+// Given 16 -bytes secret key, 16 -bytes public message nonce, N (>=0) -bytes
+// associated data & M (>=0) -bytes plain text, this routine computes M -bytes
+// ciphex text & 16 -bytes authentication tag using Photon-Beetle authenticated
+// encryption algorithm
+//
+// RATE is in terms of bytes, allowed values are {4, 16}.
+//
+// Note, avoid reusing same nonce under same secret key !
+//
+// See algorithm `PHOTON-Beetle-AEAD.ENC[r](K, N, A, M)` defined in figure 3.6
+// of Photon-Beetle specification
+// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/photon-beetle-spec-final.pdf
+template<const size_t RATE>
+inline static void
+_encrypt(
+  const uint8_t* const __restrict key,   // 16 -bytes secret key
+  const uint8_t* const __restrict nonce, // 16 -bytes public message nonce
+  const uint8_t* const __restrict data,  // N -bytes associated data | N >= 0
+  const size_t dlen,                     // len(data) >= 0
+  const uint8_t* const __restrict txt,   // N -bytes plain text | N >= 0
+  uint8_t* const __restrict enc,         // N -bytes cipher text | N >= 0
+  const size_t mlen,                     // len(txt) = len(enc) >= 0
+  uint8_t* const __restrict tag          // 16 -bytes authentication tag
+  )
+  requires(check_rate(RATE))
+{
+  uint8_t state[32]{};
+
+  std::memcpy(state, nonce, 16);
+  std::memcpy(state + 16, key, 16);
+
+  if ((dlen == 0) && (mlen == 0)) [[unlikely]] {
+    state[31] ^= (1 << 5);
+    _gen_tag<16>(state, tag);
+
+    return;
+  }
+
+  const bool f0 = mlen > 0;
+  const bool f1 = (dlen & (RATE - 1)) == 0;
+  const bool f2 = dlen > 0;
+  const bool f3 = (mlen & (RATE - 1)) == 0;
+
+  const uint8_t C0 = (f0 && f1) ? 1 : f0 ? 2 : f1 ? 3 : 4;
+  const uint8_t C1 = (f2 && f3) ? 1 : f2 ? 2 : f3 ? 5 : 6;
+
+  if (dlen > 0) [[likely]] {
+    _absorb<RATE>(state, data, dlen, C0);
+  }
+
+  if (mlen > 0) [[likely]] {
+    for (size_t off = 0; off < mlen; off += RATE) {
+      photon::_photon256(state);
+      _rho<RATE>(state, txt + off, enc + off, std::min(RATE, mlen - off));
+    }
+
+    state[31] ^= (C1 << 5);
+  }
+
+  _gen_tag<16>(state, tag);
 }
 
 // Given 16 -bytes secret key, 16 -bytes public message nonce, 16 -bytes
